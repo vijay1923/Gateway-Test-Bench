38d593b2d393161d2efd53c4d4872b51a43ed1ee
vijay1923 <vijaymagadum1923@gmail.com>
2026-02-26 18:52:31 +0530

    RS232 test

---
+++ rs232_test.h
@@ -0,0 +1,16 @@
+#ifndef RS232_TEST_h
+#define RS232_TEST_h
+#include "config.h"
+
+void rs232_test() 
+{ 
+    // Serial1.begin(9600, SERIAL_8N1, S232_RX_PIN, S232_TX_PIN);
+    // Serial.println("RS232 Test Started");
+
+     CHECK_ABORT();  // check before test   
+     Serial.println("$,RS232,2,FAIL,#"); 
+
+}
+
+
+#endif 

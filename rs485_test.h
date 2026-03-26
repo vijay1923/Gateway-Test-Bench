766bb6c7557be9d5b5502e7c8c057c4615bc6361
vijay1923 <vijaymagadum1923@gmail.com>
2026-03-26 13:17:56 +0530

    Implement 485 test function

--- rs485_test.h
+++ rs485_test.h
@@ -3,14 +3,29 @@
 
 #include "config.h"
 
+ModbusMaster node;  // Create ModbusMaster object
+
+uint8_t result;    // Variable to store Modbus result
+uint16_t value;    // Variable to store read value
 
 void rs485_test() 
 { 
-    // Serial2.begin(9600, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
-    // Serial.println("RS485 Test Started");
-    
-     CHECK_ABORT();  // check before test   
-     Serial.println("$,RS485,2,FAIL,#"); 
+ 
+    Serial2.begin(9600, SERIAL_8N1, RS485_TX, RS485_RX);
+    delay(50);
+    node.begin(1, Serial2);    // Modbus slave ID 1
+    result = node.readHoldingRegisters(0, 1);   // Read 1 register starting at address 0
+    if(result == node.ku8MBSuccess)
+    {
+        value = node.getResponseBuffer(0);
+        Serial.println("$,RS485,1,PASS,VALUE: " + String(value));
+    }
+    else
+    {
+        Serial.println("$,RS485,2,FAIL,ERROR CODE : " + String(result) + ",#");
+    }
+   
+
 
 }
 

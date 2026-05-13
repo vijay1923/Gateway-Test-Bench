220dc0900d6c2d67ef509883c7a054d90a16f044
vijay1923 <vijaymagadum1923@gmail.com>
2026-05-13 13:21:31 +0530

    Update ethernet_test.h

--- ethernet_test.h
+++ ethernet_test.h
@@ -15,18 +15,21 @@ void ethernet_test()
 { 
     Serial.println("Ethernet Test Started");
      CHECK_ABORT();  // check before test 
+    bool pass = false;
 
     SPI.begin();
     Ethernet.init(ETH_CS_PIN); // set CS pin for SPI
 
     if (Ethernet.begin(macAddr) == 0)  // DHCP fail
     {
+        update_test_result(TEST_ETHERNET, false);
         Serial.println("$,ETHERNET,2,FAIL,DHCP FAIL,#");  /// send fail responce
         return;
     }
      ABORTABLE_DELAY(200);
     if (Ethernet.linkStatus() == LinkON)    // check for link status
     {
+        pass = true;
         IPAddress ip = Ethernet.localIP();
 
         Serial.printf("$,ETHERNET,1,PASS,%d.%d.%d.%d,#\n", ip[0], ip[1], ip[2], ip[3]);
@@ -35,6 +38,8 @@ void ethernet_test()
     {
         Serial.println("$,ETHERNET,2,FAIL,NO LINK,#");
     }
+
+    update_test_result(TEST_ETHERNET, pass);
 
      CHECK_ABORT();  // ceck aftre test
     ABORTABLE_DELAY(100);

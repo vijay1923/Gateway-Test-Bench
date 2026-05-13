ad6e22e6b61e377b310763df26070f7e0befa815
vijay1923 <vijaymagadum1923@gmail.com>
2026-05-13 13:26:58 +0530

    add function to dens data over rs232

--- rs232_test.h
+++ rs232_test.h
@@ -3,15 +3,35 @@
 
 #include "config.h"
 
+// RS232 Test using Serial1
+// This test sends a message over Serial1 and waits for a response to verify RS232 communication.
+
 void rs232_test() 
 { 
-    // Serial1.begin(9600, SERIAL_8N1, RS232_RX_PIN, RS232_TX_PIN);
-    // Serial.println("RS232 Test Started");
+    bool pass = false;
+    Serial1.begin(9600, SERIAL_8N1, RS232_RX, RS232_TX);
+    Serial1.println("RS232 Test Message");
 
-     CHECK_ABORT();  // check before test   
-    update_test_result(TEST_RS232, false);
-     Serial.println("$,RS232,2,FAIL,#"); 
+    unsigned long start = millis();
+    while (Serial1.available() == 0 && millis() - start < 5000)
+    {
+        CHECK_ABORT();
+        ABORTABLE_DELAY(10);
+    }
+    if (Serial1.available() > 0)   // Response received
+    {
+        pass = true;
+        String response = Serial1.readStringUntil('\n');  
+        Serial.println("$,RS232,1,PASS,RESPONSE: " + response + ",#"); 
+    } 
+    else  
+    {
+        Serial.println("$,RS232,2,FAIL,NO RESPONSE,#"); 
+    }
 
+    update_test_result(TEST_RS232, pass);
+
+    CHECK_ABORT();
 }
 
 

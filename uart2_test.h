d0061e55ea1c580fecb5d059cfc0e8309bcbaaef
vijay1923 <vijaymagadum1923@gmail.com>
2026-05-13 13:21:36 +0530

    Update uart2_test.h

--- uart2_test.h
+++ uart2_test.h
@@ -8,6 +8,7 @@ void uart2_test()
 {
     Serial.println("UART2 Test Started");
     CHECK_ABORT();  // check before test 
+    bool pass = false;
 
     Serial2.begin(SERIAL_BAUD, SERIAL_8N1, UART2_RX, UART2_TX);
     Serial2.println("UART2 Test Message");
@@ -20,6 +21,7 @@ void uart2_test()
     }
     if (Serial2.available() > 0)   // Response received
     {
+        pass = true;
         String response = Serial2.readStringUntil('\n');  
         Serial.println("$,UART2,1,PASS,RESPONSE: " + response); 
     } 
@@ -27,6 +29,8 @@ void uart2_test()
     {
         Serial.println("$,UART2,2,FAIL,NO RESPONSE,#"); 
     }
+
+    update_test_result(TEST_UART2, pass);
 
     CHECK_ABORT();
 

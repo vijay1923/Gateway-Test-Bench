d9f01e392141d91911091e3ddb38ede775e6fdea
vijay1923 <vijaymagadum1923@gmail.com>
2026-09-05 11:01:31 +0530

    add:printer test

---
+++ printer_test.h
@@ -0,0 +1,18 @@
+#ifndef PRINTER_TEST_H
+#define PRINTER_TEST_H
+
+#include<TSPL_Printer.h>
+
+void printer_setup()
+{
+    // TSPL_Printer printer;
+    
+}
+void test_printer()
+{
+    return;
+}
+
+
+
+#endif // PRINTER_TEST_H
